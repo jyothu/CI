40ff71b1add0dc5e1a19c71a8f75775c0fe2ed99
Jyothu <jyothu.kr@gmail.com>
2015-04-10 09:52:20 +0530

    Initail Commit

---
+++ application/views/home/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8'>
+    <meta content='IE=Edge,chrome=1' http-equiv='X-UA-Compatible'>
+    <meta content='width=device-width, initial-scale=1.0' name='viewport'>
+    <title>MERMAID MARINE SERVICES</title>
+    <meta content="authenticity_token" name="csrf-param" />
+    <meta content="NqV3j0qa9mKTCqDLQzP5jeEH5eNVla867Tr0BuchOXM=" name="csrf-token" />
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>  <![endif]-->
+    <!-- = javascript_include_tag "html5shiv" -->
+    <!-- = favicon_link_tag 'favicon.ico', :rel => 'shortcut icon' -->
+    <script src="assets/jquery.js?body=1" type="text/javascript"></script>
+    <script src="assets/jquery-ui.js?body=1" type="text/javascript"></script>
+    <script src="assets/bootstrap.js?body=1" type="text/javascript"></script>
+    <script src="assets/bjqs.js?body=1" type="text/javascript"></script>
+    <script src="assets/index-slider.js?body=1" type="text/javascript"></script>
+    <script src="assets/flexslider.js?body=1" type="text/javascript"></script>
+    <script src="assets/theme.js?body=1" type="text/javascript"></script>
+    <script src="assets/jquery_ujs.js?body=1" type="text/javascript"></script>
+    <script src="assets/html5shiv.js?body=1" type="text/javascript"></script>
+    <script src="assets/application.js?body=1" type="text/javascript"></script>
+    <link href="assets/application.css?body=1" media="all" rel="stylesheet" type="text/css" />
+    <link href="assets/active_admin.css?body=1" media="all" rel="stylesheet" type="text/css" />
+    <link href="assets/animate.css?body=1" media="all" rel="stylesheet" type="text/css" />
+    <link href="assets/bjqs.css?body=1" media="all" rel="stylesheet" type="text/css" />
+    <link href="assets/bootstrap-overrides.css?body=1" media="all" rel="stylesheet" type="text/css" />
+    <link href="assets/bootstrap.css?body=1" media="all" rel="stylesheet" type="text/css" />
+    <link href="assets/contact.css?body=1" media="all" rel="stylesheet" type="text/css" />
+    <link href="assets/css.css?body=1" media="all" rel="stylesheet" type="text/css" />
+    <link href="assets/flexslider.css?body=1" media="all" rel="stylesheet" type="text/css" />
+    <link href="assets/index.css?body=1" media="all" rel="stylesheet" type="text/css" />
+    <link href="assets/theme.css?body=1" media="all" rel="stylesheet" type="text/css" />
+  </head>
+  <body class='pull_top'>
+    <div class='navbar navbar-fixed-top navbar-inverse' role='navigation'>
+      <div class='container'>
+        <div class='navbar-header'>
+          <button class='navbar-toggle pull-right' data-target='.navbar-ex1-collapse' data-toggle='collapse' type='button'>
+            <span class='sr-only'>Toggle navigation</span>
+            <span class='icon-bar'></span>
+            <span class='icon-bar'></span>
+            <span class='icon-bar'></span>
+          </button>
+          <a class='logo' href='/'>
+            <img alt="Logo" height="60" src="assets/logo.png" width="350" />
+          </a>
+        </div>
+        <div class='collapse navbar-collapse navbar-ex1-collapse' role='navigation'>
+          <ul class='nav navbar-nav navbar-right'>
+            <li class='active'>
+              <a href='index.html'>HOME</a>
+            </li>
+            <li class=''>
+              <a href='aboutus.html'>ABOUT US</a>
+            </li>
+            <li class=''>
+              <a href='services.html'>SERVICES</a>
+            </li>
+            <li class=''>
+              <a href='contactus.php'>CONTACT US</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class='text-justify'>
+      <section id=''>
+        <div class='banner'>
+          <div class='banner-slider'>
+            <ul class='bjqs'>
+              <li><img alt="Slider1" src="assets/slider1.jpg" /></li>
+              <li><img alt="Slider2" src="assets/slider2.jpg" /></li>
+              <li><img alt="Slider3" src="assets/slider3.jpg" /></li>
+              <li><img alt="Slider4" src="assets/slider4.jpg" /></li>
+              <li><img alt="Slider5" src="assets/slider5.jpg" /></li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <div id='showcase'>
+        <div class='container'>
+          <div class='row feature_wrapper'>
+            <p>
+              Mermaid Marine Services is a recruitment company founded in 2013 specializing in the marine industry. Here at Mermaid Marine Services we pride ourselves on finding world class candidates for companies operating within the Marine Industry. We work hard to carefully build strong, personal working relationships with both candidates and companies alike whether you're recruiting or looking for work within the marine industry, rest assured. Mermaid Marine Services can help with a personal and specialist service. We work closely with the world’s leading shipping, maritime and marine employers and deliver the best. Each of our consultants works in a discrete market sector and geographical region, giving you access to unrivalled knowledge and close relationships with key employers
+            </p>
+            <p>
+              So please have a look of our website and see what we can offer you. If you are a candidate then check out our selection of the best jobs from around the marine industry, sign up for our newsletter or register your CV with us. If you are a company looking to recruit then please contact us. Whatever your recruitment need is please get in touch.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- starts footer -->
+    <footer id='footer'>
+      <div class='container text-center'>
+        © 2013 Mermaid Marine Services. All rights reserved.
+      </div>
+    </footer>
+        <script type="text/javascript">
+      $(document).ready(function(){
+        $('.banner').bjqs({height: 448, width: 1366, responsive: true });
+      });
+    </script>
+  </body>
+</html>
